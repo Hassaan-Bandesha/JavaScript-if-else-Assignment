aefad3af5383bd5836642584113377e34855c0b1
Hassaan-Bandesha <hassaan4742@gmail.com>
2024-03-03 21:47:05 +0500

    JavaScript if else Assignment created

---
+++ app.js
@@ -0,0 +1,74 @@
+                //   Question # 1
+
+// var userNum1 = +prompt('Enter your number!');
+// if (userNum1 % 3 === 0){
+//     console.log('Number is divisible by 3')
+// }
+// else{
+//     console.log('Number is not divisible by 3');
+// }
+
+
+                // Question # 2
+            
+// var userNum2 = +prompt('Enter your Number');
+// if (userNum2 % 2 === 0){
+//     console.log('The given number is even')
+// }
+// else{
+//     console.log('The given number is odd')
+// }
+
+
+              // Question # 3
+              
+// var userAge = +prompt('Enter your age');
+// if (userAge > 18){
+//     console.log('Old enough')
+// }
+// else{
+//     console.log('Too young')
+// }
+
+
+            //   Question # 4
+
+// var userName = prompt('Enter your name');
+// if (userName === 'Hassaan'){
+//     console.log('Welcome to our website')
+// }
+// else{
+//     console.log('')
+// }
+
+            //   Question # 5
+
+//  var userNum3 = +prompt('Enter your number');
+//  switch (userNum3 % 3 === 0){
+//     case true:
+//         console.log('Number is divisble by 3')
+//     break;
+//     default:
+//         console.log('Number is not divisble by 3')
+//  }
+
+                // Queation # 6
+
+// let word = prompt('Enter your number or word.')
+// for (i = 0; i < word.length; i++){
+//     let asci = word.charCodeAt(i);
+//     console.log(asci);
+//     let asci1 = asci;
+//     if (asci1 >= 65 && asci1 <= 90){           //ASCI Value A to Z range from 65 to 90 
+//         console.log('This is uppercase word.')
+//     }
+//     else if (asci1 >= 97 && asci1 <= 122){     //ASCI Value a to z range from 97 to 122 
+//         console.log('This is lowercase word.')
+//     }
+//     else if (asci1 >= 48 && asci1 <= 57){      //ASCI Value 0 to 9 range from 48 to 57
+//         console.log('This is a Number.')
+//     }
+//     else{
+//         console.log('This is not a number nor a word.')
+//     }
+// }
